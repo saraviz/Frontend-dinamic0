bf322e8ac886bc878398ec1179efdeee260d138b
Sara Aviz <123568610+saraviz@users.noreply.github.com>
2024-01-31 15:24:10 -0300

    Update index.js
    Implementação da API

--- entrega/index.js
+++ entrega/index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const registerButton = document.getElementById('register-button');
-    const registrationForm = document.getElementById('registration-form');
 
-    registerButton.addEventListener('click', function(event) {
+    registerButton.addEventListener('click', async function(event) {
         event.preventDefault(); // Impede o envio do formulário
 
         // Validar campos do formulário
         const name = document.getElementById('name').value.trim();
         const email = document.getElementById('email').value.trim();
         const postalCode = document.getElementById('postal-code').value.trim();
+        const optIn = document.getElementById('consent-checkbox').checked;
 
         // Validar e-mail
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,7 +26,47 @@ document.addEventListener('DOMContentLoaded', function() {
             return; // Impede o envio do formulário se o e-mail for inválido
         }
 
-       
+        // Salvar os dados no localStorage
+        localStorage.setItem('name', name);
+        localStorage.setItem('email', email);
+        localStorage.setItem('postal-code', postalCode);
+        localStorage.setItem('optIn', optIn);
+
+        // Imprimir os valores do localStorage no console para verificar
+        console.log(localStorage.getItem('name'));
+        console.log(localStorage.getItem('email'));
+        console.log(localStorage.getItem('postal-code'));
+        console.log(localStorage.getItem('optIn'));
+
+        const formObject = {
+            name: name,
+            email: email,
+            postalCode: postalCode,
+            optIn: optIn
+        };
+
+        try {
+            const response = await fetch('https://churrascometro-api.vercel.app/leads', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formObject),
+            });
+
+            if (response.ok) {
+                // flag para identificar se já foi enviado o form
+                localStorage.setItem('formSubmitted', 'true');
+                window.location.href = './result.html';
+            } else {
+                alert('Erro ao enviar os dados para a API. Por favor, tente novamente.');
+            }
+        } catch (error) {
+            console.error('Erro ao enviar os dados para a API:', error);
+            alert('Erro ao enviar os dados para a API. Por favor, tente novamente.');
+        }
+
+        // Redirecionar para a próxima página
         window.location.href = './pages/segunda_pagina.html';
     });
 });
